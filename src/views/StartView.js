3b13df7a2005971d46614c8b1856cb82021f27a9
Xinyi Wang <is.xinyiwang@outlook.com>
2021-04-15 18:16:36 +0200

    edit start page

--- src/views/StartView.js
+++ src/views/StartView.js
@@ -1,15 +1,22 @@
 // src/views/StartView.js
 import Button from 'react-bootstrap/Button';
+import "./StartView.scss";
 
 function StartView(props){
     return(
-        <div className="App">
-          <p>Firebase config options</p>
+        <div className="App" >
+          {/* <p>Firebase config options</p>
           <code>
             <pre>{props.firebaseOptions}</pre>
-          </code>
-          <Button variant="secondary" href={props.loginUrl}>
+          </code> */}
+          <p>{console.log(`Firebase config options: ${props.firebaseOptions}`)}</p>
+          <header className="login-header">
+            <h1>Listify</h1>
+          </header>
+          <Button variant="secondary" href={props.loginUrl} className="login-bnt">
+          <i class="fa fa-spotify">
             Login with Spotify
+            </i>
           </Button>
         </div>
     )
