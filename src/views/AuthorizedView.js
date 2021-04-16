053f581b3cfa4110ce4bfd302c589f8a4aee62cf
RamiAchkoudir <35962350+RamiAchkoudir@users.noreply.github.com>
2021-04-16 16:37:39 +0200

    Initial layout of the AuthorizedView

--- src/views/AuthorizedView.js
+++ src/views/AuthorizedView.js
@@ -1,9 +1,81 @@
 // src/views/AuthorizedView.js
+import './AuthorizedView.scss';
+import Table from 'react-bootstrap/Table'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
+import React from 'react'
+
 
 function AuthorizedView(props){
     return (
         <div className="App">
-            <p>Login success, authorized through Spotify!</p>
+        <Tabs className="tabs" defaultActiveKey="tracks" id="home-page-tabs">
+            <Tab eventKey="playlist" title="Playlists">
+                <p>Will display playlists eventually</p>
+            </Tab>
+            <Tab eventKey="tracks" title="Tracks">
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Artist</th>
+                            <th>Song Name</th>
+                            <th>Album</th>
+                        </tr>
+                    </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>The Weeknd</td>
+                                <td>Blinding Lights</td>
+                                <td>After Hours</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>The Weeknd</td>
+                                <td>In Your Eyes</td>
+                                <td>After Hours</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Justin Bieber</td>
+                                <td>Peaches</td>
+                                <td>Justice</td>
+                             </tr>
+                             <tr>
+                                <td>4</td>
+                                <td>Molly Sandén</td>
+                                <td>Nån annan nu</td>
+                                <td>Nån annan nu</td>
+                             </tr>
+                             <tr>
+                                <td>5</td>
+                                <td>Lil Nas X</td>
+                                <td>Montero</td>
+                                <td>Montero</td>
+                             </tr>
+                             <tr>
+                                <td>6</td>
+                                <td>Alesso</td>
+                                <td>Going Dumb</td>
+                                <td>Going Dumb</td>
+                             </tr>
+                             <tr>
+                                <td>7</td>
+                                <td>Tusse</td>
+                                <td>Voices</td>
+                                <td>Voices</td>
+                             </tr>
+                        </tbody>
+                </Table>
+
+            </Tab>
+            <Tab eventKey="search" title="Search">
+            <p>Search</p>
+            </Tab>
+            <Tab className="listify" eventKey="logo" title="Listify" disabled></Tab>
+        </Tabs>  
+
         </div>
   );
 }
